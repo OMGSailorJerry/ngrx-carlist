3bd95a4b2b9900a4359616731150eb44668faf07
Oleg Mikhailov <omikhailov@ace-st.com>
2018-03-16 12:24:50 +0200

    Store registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { CarsFromComponent } from './cars-from/cars-from.component';
 import { CarComponent } from './car/car.component';
+import { CarsReducer } from './redux/cars.reducer';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { CarComponent } from './car/car.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    StoreModule.forRoot({ carPage: CarsReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
